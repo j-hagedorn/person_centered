828a56392ff1b600f29327957749573149d63c87
jenna-dietsch <30694833+jenna-dietsch@users.noreply.github.com>
2021-03-15 15:26:43 -0400

    update prep_regs.R
    move fidelity requirements section from regulate to person_centered repo; define path for Teams documents

--- prep/prep_regs.R
+++ prep/prep_regs.R
@@ -1,10 +1,19 @@
 ## prep_regs.R
 
+tbdsuser <- Sys.info()[["login"]]
+
 # group concepts from vocab
 source("prep/concepts.R")
 
 # content analysis
 source("prep/cooccur.R")
+
+# fidelity requirements
+path <- paste0("C:/Users/",tbdsuser,"/TBD Solutions LLC/PCP Framework - General/reg_refs")
+
+source("prep/make_pcp_reg_docs.R") # formats data as snippets of regs and creates .doc files
+source("prep/read_pcp_reg_docs.R") # associates fidelity requirements entered manually into .doc files with source documents
+source("prep/join_regs_to_concepts.R") # combine requirements with concepts
 
 # write necessary files to app data folder
 write_csv(corpus_concepts,"app/data/corpus_concepts.csv")
